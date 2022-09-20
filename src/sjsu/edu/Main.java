f4cb6f6ff5cb8bff3aab5086eb638fd56d5e2183
Khaled Zaza <khaledzaza.cs@gmail.com>
2022-09-20 15:12:41 -0700

    Added ArrayList instead of array because wanted it to be flexible to file sizes

--- src/sjsu/edu/Main.java
+++ src/sjsu/edu/Main.java
@@ -2,6 +2,7 @@ package sjsu.edu;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,27 +13,35 @@ public class Main {
 
 
         Scanner scanner = new Scanner(new File("src/sjsu/edu/arr1-2.txt"));
-        int [] arr1 = new int [50];
-        int x = 0;
+//        int [] arr1 = new int [50];
+//        int x = 0;
+//        while(scanner.hasNextInt()){
+//            arr1[x++] = scanner.nextInt();
+//        }
+//        int size = arr1.length;
+//
+//
+//        System.out.println(Arrays.toString(arr1));
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
         while(scanner.hasNextInt()){
-            arr1[x++] = scanner.nextInt();
+            arr1.add(scanner.nextInt());
         }
-        int size = arr1.length;
 
 
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(arr1);
+        int size = arr1.size();
 
         for (int i= size-1; i>=1; i--) {
             for (int j = 1; j <= i ; j++) {
-                if (arr1[j - 1] > arr1[j]) {
-                    int temp = arr1[j];
-                    arr1[j] = arr1[j - 1];
-                    arr1[j - 1] = temp;
+                if (arr1.get(j - 1) > arr1.get(j)) {
+                    int temp = arr1.get(j);
+                    arr1.set(j, arr1.get(j - 1));
+                    arr1.set(j - 1, temp);
                 }
             }
         }
 
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(arr1);
 
 
     }
